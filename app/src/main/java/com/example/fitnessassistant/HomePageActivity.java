a8a312e1429a124ccb3d356f14e60696a5c5e4e3
Marko-Gordic <71905426+Marko-Gordic@users.noreply.github.com>
2021-11-04 00:46:49 +0100

    Minor bugs fixed, Home Page updated. The initial version of the step tracker implemented.

--- app/src/main/java/com/example/fitnessassistant/HomePageActivity.java
+++ app/src/main/java/com/example/fitnessassistant/HomePageActivity.java
@@ -1,5 +1,10 @@
 package com.example.fitnessassistant;
 
+import android.content.SharedPreferences;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -11,10 +16,46 @@ import java.util.GregorianCalendar;
 
 public class HomePageActivity extends AppCompatActivity {
 
+    double MagnitudePrevious = 1;
+    int stepCount = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_page);
+
+        TextView stepCountText = findViewById(R.id.stepCount);
+
+        SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        Sensor sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        SensorEventListener stepDetector = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                if (event != null){
+                    float x_acceleration = event.values[0];
+                    float y_acceleration = event.values[1];
+                    float z_acceleration = event.values[2];
+
+                    double Magnitude = Math.sqrt(x_acceleration*x_acceleration + y_acceleration*y_acceleration + z_acceleration*z_acceleration);
+                    double MagnitudeDelta = Magnitude - MagnitudePrevious;
+                    MagnitudePrevious = Magnitude;
+
+                    if (MagnitudeDelta > 4){
+                        stepCount++;
+                    }
+
+                    stepCountText.setText(String.valueOf(stepCount));
+                }
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+            }
+        };
+
+        sensorManager.registerListener(stepDetector, sensor, SensorManager.SENSOR_DELAY_NORMAL);
 
         TextView welcome = findViewById(R.id.welcomeMessage); // TextView in top right corner for welcome message
 
@@ -33,5 +74,33 @@ public class HomePageActivity extends AppCompatActivity {
         else if((systemHours >= 23 && systemHours <= 24) || (systemHours >= 0 && systemHours < 6))
             welcome.setText("Good Night, ");
 
+
+    }
+
+    protected void onPause() {
+        super.onPause();
+
+        SharedPreferences sharedPreferences = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.clear();
+        editor.putInt("stepCount", stepCount);
+        editor.apply();
+    }
+
+    protected void onStop() {
+        super.onStop();
+
+        SharedPreferences sharedPreferences = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.clear();
+        editor.putInt("stepCount", stepCount);
+        editor.apply();
+    }
+
+    protected void onResume() {
+        super.onResume();
+
+        SharedPreferences sharedPreferences = getPreferences(MODE_PRIVATE);
+        stepCount = sharedPreferences.getInt("stepCount", 0);
     }
 }
