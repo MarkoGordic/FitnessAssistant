b0c87c2e5436c0fc7ce3124edc1a309b6e0aa4a8
Marko-Gordic <71905426+Marko-Gordic@users.noreply.github.com>
2021-11-04 22:00:15 +0100

    Removed old pedometer, started new pedometer implementation. Added new required permissions.

--- app/src/main/java/com/example/fitnessassistant/HomePageActivity.java
+++ app/src/main/java/com/example/fitnessassistant/HomePageActivity.java
@@ -1,10 +1,5 @@
 package com.example.fitnessassistant;
 
-import android.content.SharedPreferences;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -15,44 +10,11 @@ import java.util.Date;
 import java.util.GregorianCalendar;
 
 public class HomePageActivity extends AppCompatActivity {
-    private double MagnitudePrevious = 1;
-    private int stepCount;
-    private SharedPreferences prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_page);
-        prefs = getSharedPreferences("StepCounter", MODE_PRIVATE);
-        stepCount = 0;
-
-        SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        Sensor sensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
-
-        SensorEventListener stepDetector = new SensorEventListener() {
-            @Override
-            public void onSensorChanged(SensorEvent event) {
-                if (event != null){
-                    float x_acceleration = event.values[0];
-                    float y_acceleration = event.values[1];
-                    float z_acceleration = event.values[2];
-
-                    double Magnitude = Math.sqrt(x_acceleration*x_acceleration + y_acceleration*y_acceleration + z_acceleration*z_acceleration);
-                    double MagnitudeDelta = Magnitude - MagnitudePrevious;
-                    MagnitudePrevious = Magnitude;
-
-                    if (MagnitudeDelta > 4)
-                        stepCount++;
-
-                    ((TextView) findViewById(R.id.stepCount)).setText(String.valueOf(stepCount));
-                }
-            }
-
-            @Override
-            public void onAccuracyChanged(Sensor sensor, int accuracy) {}
-        };
-
-        sensorManager.registerListener(stepDetector, sensor, SensorManager.SENSOR_DELAY_UI);
 
         TextView welcome = findViewById(R.id.welcomeMessage); // TextView in top right corner for welcome message
 
@@ -72,22 +34,4 @@ public class HomePageActivity extends AppCompatActivity {
             welcome.setText(getString(R.string.good_night));
     }
 
-    protected void onPause() {
-        super.onPause();
-        SharedPreferences.Editor editor = prefs.edit();
-        editor.putInt("stepCount", stepCount);
-        editor.apply();
-    }
-
-    protected void onStop() {
-        super.onStop();
-        SharedPreferences.Editor editor = prefs.edit();
-        editor.putInt("stepCount", stepCount);
-        editor.apply();
-    }
-
-    protected void onResume() {
-        super.onResume();
-        stepCount = prefs.getInt("stepCount", 0);
-    }
 }
---
+++ app/src/main/java/com/example/pedometer/ShutdownReceiver.java
@@ -0,0 +1,12 @@
+package com.example.pedometer;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class ShutdownReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
