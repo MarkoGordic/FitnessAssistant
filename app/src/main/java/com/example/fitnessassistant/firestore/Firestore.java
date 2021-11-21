aa359a46e9547cb62b1ef42fd9a16004cdc24b49
Marko-Gordic <mgordic22@gmail.com>
2021-11-21 17:47:05 +0100

    Firestore integration updated.

--- app/src/main/java/com/example/fitnessassistant/firestore/Firestore.java
+++ app/src/main/java/com/example/fitnessassistant/firestore/Firestore.java
@@ -1,9 +1,15 @@
 package com.example.fitnessassistant.firestore;
 
+import android.annotation.SuppressLint;
 import android.util.Log;
 
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -11,15 +17,41 @@ import java.util.concurrent.atomic.AtomicBoolean;
 public class Firestore {
     FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-    AtomicBoolean success = new AtomicBoolean(false);
+    public static AtomicBoolean success = new AtomicBoolean(false);
 
-    public void createNewUser(String userID){
+    public static void createNewUser(String userID){
         Map<String, Object> user = new HashMap<>();
         user.put("id", userID);
 
-        db.collection("users")
+        FirebaseFirestore.getInstance().collection("users")
                 .add(user)
                 .addOnSuccessListener(documentReference -> success.set(true))
                 .addOnFailureListener(e -> Log.w("Error while adding document!", e));
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    public static void createPedometerData(String userID){
+        Calendar calendar;
+        SimpleDateFormat dateFormat;
+        String dateText;
+
+        calendar = Calendar.getInstance();
+
+        dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+        dateText = dateFormat.format(calendar.getTime());
+
+        Map<String, Object> pedometerData = new HashMap<>();
+        pedometerData.put("date", dateText);
+        pedometerData.put("steps", 0);
+
+        DocumentReference userData = FirebaseFirestore.getInstance().collection("users").document(userID).collection("pedometer").document(dateText);
+        Task<DocumentSnapshot> document = userData.get();
+        if(document.getResult().exists()){
+            userData.update(pedometerData)
+                    .addOnFailureListener(e -> Log.w("Error while adding document!", e));
+        }
+        else{
+            FirebaseFirestore.getInstance().collection("users").document(userID).collection("pedometer").add(pedometerData);
+        }
     }
 }
