c6fb0fd3e2cf384bfe67ce96b4282fb4f545d462
mgordic <mgordic22@gmail.com>
2021-12-19 01:32:15 +0100

    Database functionality added.
    - Realtime Database connection added
    - Method for adding new user to database is added
    - google-services.json file updated

--- app/src/main/java/com/example/fitnessassistant/database/RealtimeDB.java
+++ app/src/main/java/com/example/fitnessassistant/database/RealtimeDB.java
@@ -5,45 +5,31 @@ import androidx.annotation.NonNull;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class RealtimeDB {
+
+    // method for adding new using to database
+    // !! CURRENTLY DISABLED, but working
     public static void registerNewUser(){
         if(FirebaseAuth.getInstance().getCurrentUser() != null) {
             String userID = FirebaseAuth.getInstance().getCurrentUser().getUid();
-            DatabaseReference currUser = FirebaseDatabase.getInstance().getReference().child("users").child(userID);
+            DatabaseReference db = FirebaseDatabase.getInstance("https://fitness-assistant-app-default-rtdb.europe-west1.firebasedatabase.app").getReference("users");
             String username = FirebaseAuth.getInstance().getCurrentUser().getDisplayName();
-            currUser.child("Username").setValue("Test123")
-            .addOnCompleteListener(task -> {
-                try {
-                    if (!task.isSuccessful()) {
-                        if (task.getException() != null)
-                            throw task.getException();
-                    }
-                    else{
-                        System.out.println("Registration successful!");
-                    }
-                } catch (Exception e){
-                    e.printStackTrace();
+
+            db.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    db.child(userID).child("username").setValue(username);
                 }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) { }
             });
-        }
-    }
-
-    public static void removeUser(){
-        if(FirebaseAuth.getInstance().getCurrentUser() != null) {
-            String userID = FirebaseAuth.getInstance().getCurrentUser().getUid();
-            DatabaseReference currUser = FirebaseDatabase.getInstance().getReference().child("users").child(userID);
-            currUser.removeValue();
-        }
-    }
-
-    public static void updateUsername(){
-        if(FirebaseAuth.getInstance().getCurrentUser() != null) {
-            String userID = FirebaseAuth.getInstance().getCurrentUser().getUid();
-            DatabaseReference currUser = FirebaseDatabase.getInstance().getReference().child("users").child(userID);
-            currUser.removeValue();
         }
     }
 
