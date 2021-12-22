5b9003e22b9cb27c863f155d63b71b4915ec6ce3
IgorMaljik <imaljik7@gmail.com>
2021-12-22 09:33:50 +0100

    Removed unecessary files from previous commit

--- app/src/main/java/com/example/fitnessassistant/pedometer/PedometerWidget.java
+++ app/src/main/java/com/example/fitnessassistant/pedometer/PedometerWidget.java
@@ -7,37 +7,17 @@ import android.widget.RemoteViews;
 
 import com.example.fitnessassistant.R;
 
-/**
- * Implementation of App Widget functionality.
- */
 public class PedometerWidget extends AppWidgetProvider {
 
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-                                int appWidgetId) {
-
-
-        // Construct the RemoteViews object
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.pedometer_widget);
-
-        // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        // There may be multiple widgets active, so update all of them
         for (int appWidgetId : appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId);
         }
-    }
-
-    @Override
-    public void onEnabled(Context context) {
-        // Enter relevant functionality for when the first widget is created
-    }
-
-    @Override
-    public void onDisabled(Context context) {
-        // Enter relevant functionality for when the last widget is disabled
     }
 }
