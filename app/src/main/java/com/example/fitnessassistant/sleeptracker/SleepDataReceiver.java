e7bcc1e5449d777c7218165c2ab06abdb2f5860d
Marko-Gordic <mgordic22@gmail.com>
2022-01-20 18:17:19 +0100

    DB update
    - Sleep DB updated

--- app/src/main/java/com/example/fitnessassistant/sleeptracker/SleepDataReceiver.java
+++ app/src/main/java/com/example/fitnessassistant/sleeptracker/SleepDataReceiver.java
@@ -25,7 +25,7 @@ public class SleepDataReceiver extends BroadcastReceiver {
                 Calendar cal = Calendar.getInstance();
                 cal.setTimeInMillis(sleepEvents.get(i).getEndTimeMillis());
                 String date = (String) DateFormat.format("yyyyMMdd", cal);
-                MDBHSleepTracker.getInstance(context).addNewSleepSegment(context, sleepEvents.get(i).getStartTimeMillis(), sleepEvents.get(i).getEndTimeMillis(), date, null, null);
+                MDBHSleepTracker.getInstance(context).addNewSleepSegment(context, sleepEvents.get(i).getStartTimeMillis(), sleepEvents.get(i).getEndTimeMillis(), date, null, null, false);
                 System.out.println(sleepEvents.get(i) + " SLEEP_DATA");
             }
         }
